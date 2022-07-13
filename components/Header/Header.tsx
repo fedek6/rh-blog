4449813bcbae490b1f5d5a2d05c4259b331568b3
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-13 23:46:09 +0200

    Update header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Container } from "./Header.styles";
 import { Logo } from "@/components/Logo";
 import { ThemeSwitch } from "@/components/ThemeSwitch";
+import { siteMeta } from "@/config/meta";
 
 type Props = {};
 
 export const Header: React.FC<Props> = () => {
   return (
     <Container>
-      <Logo />
+      <Logo size="h-10" aria-label={siteMeta.name} />
       <ThemeSwitch />
     </Container>
   );
