ad606565317e52a42c875d44988069b596fe3d5b
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-13 23:43:49 +0200

    Disable prefixing classes in SVG imports

--- next.config.js
+++ next.config.js
@@ -9,7 +9,21 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "prefixIds",
+                  active: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
     });
 
     return config;
