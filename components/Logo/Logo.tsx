f511d12fb8c1ba840e9548a7355d812727cf565d
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-13 23:44:11 +0200

    Update Logo component

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -2,13 +2,17 @@ import * as React from "react";
 import Link from "next/link";
 import RHLogo from "./logo.svg";
 
-type Props = {};
+type Props = {
+  size: "h-10" | "h-8";
+};
 
-export const Logo: React.FC<Props> = ({ ...props }) => {
+type NativeProps = React.ComponentPropsWithoutRef<"a">;
+
+export const Logo: React.FC<Props & NativeProps> = ({ size, ...props }) => {
   return (
     <Link href="/">
-      <a>
-        <RHLogo />
+      <a {...props}>
+        <RHLogo className={`fill-eerie-black dark:fill-white ${size}`} />
       </a>
     </Link>
   );
