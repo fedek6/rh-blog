f5474a69d2c0d3177ba899c2bc0159ddd8629379
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-13 12:00:00 +0200

    Add additional config with body classes

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import { Container } from "./Layout.styles";
+import { bodyClasses } from "@/config/theme";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const Layout: React.FC<Props> = ({ children }) => (
-  <Container>{children}</Container>
-);
+export const Layout: React.FC<Props> = ({ children }) => {
+  React.useEffect(() => {
+    bodyClasses.map((c) => document.body.classList.add(c));
+  });
+
+  return <Container>{children}</Container>;
+};
---
+++ config/theme.ts
@@ -0,0 +1,7 @@
+export const bodyClasses = [
+  "bg-white",
+  "text-black",
+  "antialiased",
+  "dark:bg-gray-900",
+  "dark:text-white",
+];
