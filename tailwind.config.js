75e3267f9222dbd728821f7a62be4e0bf2fb8e77
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-13 22:04:21 +0200

    Add english vermillion color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     "./config/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "english-vermillion": {
+          100: "#f7d9da",
+          200: "#efb3b5",
+          300: "#e68c8f",
+          400: "#de666a",
+          500: "#d64045",
+          600: "#ab3337",
+          700: "#802629",
+          800: "#561a1c",
+          900: "#2b0d0e",
+          DEFAULT: "#d64045",
+        },
+      },
+    },
   },
   plugins: [],
 };
