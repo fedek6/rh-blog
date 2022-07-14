501146188fc9d68669f86f32455ad8e17abd601a
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-14 10:44:18 +0200

    Add custom favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class">
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
