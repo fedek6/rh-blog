45373e08a8fe83d14766a1c9876f9f76ef7d35d3
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-14 09:51:39 +0200

    Refactor Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Container, InnerContainer } from "./Layout.styles";
 import { Header } from "@/components/Header";
-import { bodyClasses } from "@/config/theme";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const Layout: React.FC<Props> = ({ children }) => {
-  React.useEffect(() => {
-    bodyClasses.map((c) => document.body.classList.add(c));
-  });
-
   return (
     <Container>
       <InnerContainer>
