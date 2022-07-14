4605934b06ecf8a4548ff31ad531b1f4dea1f179
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-14 09:24:18 +0200

    Refactor logo

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -3,16 +3,24 @@ import Link from "next/link";
 import RHLogo from "./logo.svg";
 
 type Props = {
-  size: "h-10" | "h-8";
+  logoClasses: string;
 };
 
 type NativeProps = React.ComponentPropsWithoutRef<"a">;
 
-export const Logo: React.FC<Props & NativeProps> = ({ size, ...props }) => {
+export const Logo: React.FC<Props & NativeProps> = ({
+  logoClasses,
+  ...props
+}) => {
   return (
     <Link href="/">
-      <a {...props}>
-        <RHLogo className={`fill-eerie-black dark:fill-white ${size}`} />
+      <a className="flex items-center justify-between" {...props}>
+        <div className="mr-4">
+          <RHLogo
+            className={`fill-eerie-black dark:fill-white ${logoClasses}`}
+          />
+        </div>
+        <div className="hidden h-6 text-2xl font-semibold sm:block">blog</div>
       </a>
     </Link>
   );
