ef8cbbfebb7a646500752a6633badf3cb789da72
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-15 18:44:09 +0200

    Add CloseButton component

---
+++ components/CloseButton/CloseButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IconButton } from "@/components/primitives/IconButton";
+import Icon from "./icon.svg";
+
+type Props = {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+const ICON_CLASSES = "fill-eerie-black dark:fill-white h-12 w-12";
+
+export const CloseButton: React.FC<Props> = ({ ...props }) => {
+  return (
+    <IconButton {...props}>
+      <Icon className={ICON_CLASSES} />
+    </IconButton>
+  );
+};
