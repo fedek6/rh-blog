20a03595884b0eafabee122f0ad501fc58c23918
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-15 18:36:51 +0200

    Refactor Hamburger component

--- components/Hamburger/Hamburger.tsx
+++ components/Hamburger/Hamburger.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { IconButton } from "../primitives/IconButton";
 import Icon from "./icon.svg";
 
 type Props = {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
+const ICON_CLASSES = "fill-eerie-black dark:fill-white h-12 w-12";
+
 export const Hamburger: React.FC<Props> = ({ ...props }) => {
   return (
-    <button className="cursor-pointer" {...props}>
-      <Icon className="fill-eerie-black dark:fill-white h-12 w-12" />
-    </button>
+    <IconButton {...props}>
+      <Icon className={ICON_CLASSES} />
+    </IconButton>
   );
 };
