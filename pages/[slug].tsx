0f7e6e0c58fb5855cdc89d89fa8595ec76a78311
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-24 14:17:19 +0200

    Add slug skeleton

---
+++ pages/[slug].tsx
@@ -0,0 +1,48 @@
+import { Layout } from "@/components/Layout";
+import { CommonSEO } from "@/components/SEO";
+import { getSortedContent, getSlugs } from "@/lib/content";
+import type { NextPage, GetStaticPaths, GetStaticProps } from "next";
+import type { ParsedUrlQuery } from "querystring";
+
+interface Props extends ParsedUrlQuery {
+  slug: string;
+}
+
+const Slug: NextPage<Props> = ({ slug }) => {
+  return (
+    <Layout>
+      <CommonSEO />
+      <h1 className="text-3xl font-display font-bold text-english-vermillion">
+        {slug}
+      </h1>
+    </Layout>
+  );
+};
+
+const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = await getSlugs("blog");
+
+  const paths = slugs.map((slug) => ({
+    params: {
+      slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const params = context.params as Props;
+
+  return {
+    props: {
+      ...params,
+    },
+  };
+};
+
+export default Slug;
+export { getStaticPaths, getStaticProps };
