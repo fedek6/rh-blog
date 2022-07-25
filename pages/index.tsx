5cd5faf3b7b196f6e6b87b279a4684e89fcc807e
Konrad Fedorczyk <fedek6@gmail.com>
2022-07-25 23:13:06 +0200

    Add dummy test for new file API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import { Layout } from "@/components/Layout";
 import { CommonSEO } from "@/components/SEO";
-import type { NextPage } from "next";
+import {
+  getSortedContent,
+  getAllSlugs,
+  getAllFrontmatter,
+  sortFrontmatterByDate,
+} from "@/lib/content";
+import type { ParsedUrlQuery } from "querystring";
+import type { NextPage, GetStaticPaths, GetStaticProps } from "next";
 
-const Home: NextPage = () => {
+type Props = {
+  test: string;
+};
+
+const Home: NextPage<Props> = ({ test }) => {
   return (
     <Layout>
       <CommonSEO />
@@ -10,8 +21,26 @@ const Home: NextPage = () => {
         Hello world!
       </h1>
       <p className="font-mono">Lorem ipsum</p>
+      {test}
     </Layout>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  await getSortedContent("test");
+  const slugs = await getAllSlugs("blog");
+
+  let posts = await getAllFrontmatter("blog");
+
+  posts = sortFrontmatterByDate(posts);
+
+  // console.log(posts);
+
+  return {
+    props: {
+      test: "1",
+    },
+  };
+};
