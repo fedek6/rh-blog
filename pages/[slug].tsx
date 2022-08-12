4c0a03a708e4358a81600a2e464cd27ab036b116
Konrad Fedorczyk <fedek6@gmail.com>
2022-08-12 21:34:08 +0200

    Refactor slug

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -8,13 +8,7 @@ import {
 } from "@/lib/content";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
-import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import {
-  dark,
-  dracula,
-  prism,
-} from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { MDXComponents } from "@/components/MDXComponents";
 import type { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import type { ParsedUrlQuery } from "querystring";
 import path from "path";
@@ -33,34 +27,6 @@ interface Params extends ParsedUrlQuery {
   slug: string;
 }
 
-const components = {
-  h2: (props: any) => (
-    <h2
-      className="text-2xl font-display font-bold text-english-vermillion"
-      {...props}
-    />
-  ),
-  img: (props: any) => (
-    // eslint-disable-next-line jsx-a11y/alt-text
-    <Image {...props} layout="responsive" loading="lazy" />
-  ),
-  code({ node, inline, className, children, ...props }: any) {
-    const match = /language-(\w+)/.exec(className || "");
-    return !inline && match ? (
-      <SyntaxHighlighter
-        style={dracula} // try passing different color schemes, drak, dracula etc.
-        language={match[1]}
-        PreTag="div"
-        {...props}
-      >
-        {String(children).replace(/\n$/, "")}
-      </SyntaxHighlighter>
-    ) : (
-      <code>{children}</code>
-    );
-  },
-};
-
 const Slug: NextPage<Props> = ({ slug, post, source }) => {
   return (
     <Layout>
@@ -68,7 +34,7 @@ const Slug: NextPage<Props> = ({ slug, post, source }) => {
       <h1 className="text-3xl font-display font-bold text-english-vermillion">
         {post.title}
       </h1>
-      <MDXRemote {...source} components={components} />
+      <MDXRemote {...source} components={MDXComponents} />
     </Layout>
   );
 };
@@ -96,15 +62,12 @@ const getStaticProps: GetStaticProps<Props> = async (context) => {
   const post = file.data as Frontmatter;
   const source = await serialize(file.content, {
     mdxOptions: {
-      // use the image size plugin, you can also specify which folder to load images from
-      // in my case images are in /public/images/, so I just prepend 'public'
-
       rehypePlugins: [
         [
           setImageSrc,
           {
-            dir: "public/images/hello-world-3/",
-            url: "/images/hello-world-3/",
+            dir: path.join("public", "images", params.slug),
+            url: `/images/${params.slug}/`,
           },
         ],
       ],
