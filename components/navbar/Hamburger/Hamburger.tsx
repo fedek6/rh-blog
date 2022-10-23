67f04056cd8dc71eaf085a74dff58a22439df598
Konrad Fedorczyk <konrad.fedorczyk@oko.press>
2022-10-23 17:38:23 +0200

    Update hamburger button

--- components/navbar/Hamburger/Hamburger.tsx
+++ components/navbar/Hamburger/Hamburger.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { IconButton } from "@/components/common/IconButton";
-import Icon from "./icon.svg";
+import IconHamburger from "./icon-hamburger.svg";
 
 type Props = {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const ICON_CLASSES = "fill-eerie-black dark:fill-white h-12 w-12";
-
 export const Hamburger: React.FC<Props> = ({ ...props }) => {
   return (
-    <IconButton {...props}>
-      <Icon className={ICON_CLASSES} />
+    <IconButton aria-label="Show menu" {...props}>
+      <IconHamburger
+        className="fill-eerie-black dark:fill-platinum-100 h-6 w-6"
+        role="img"
+      />
     </IconButton>
   );
 };
