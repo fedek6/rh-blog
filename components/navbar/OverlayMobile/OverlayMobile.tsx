4dbdd0b823fd26dd2d7c17ffd9cf033cfd9f0e87
Konrad Fedorczyk <konrad.fedorczyk@oko.press>
2022-10-27 23:40:17 +0200

    Add button logic

--- components/navbar/OverlayMobile/OverlayMobile.tsx
+++ components/navbar/OverlayMobile/OverlayMobile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Logo } from "@/components/common/Logo";
 import { BackButton } from "@/components/common/BackButton";
 import { CloseButton } from "@/components/common/CloseButton";
+import { SearchButton } from "@/components/common/SearchButton";
 import { Menu } from "@/components/navbar/Menu";
 import { MAIN_MENU } from "@/config/menu";
 import { Container } from "./OverlayMobile.styles";
@@ -24,8 +25,16 @@ export const OverlayMobile: React.FC<Props> = ({
       <div className="flex justify-between h-16 items-center px-5">
         <Logo className="h-8 lg:h-10" />
         <div className="flex gap-4">
-          {view === "search" && (
-            <BackButton onClick={console.log} aria-label="Return to menu" />
+          {view === "search" ? (
+            <BackButton
+              onClick={() => setView("menu")}
+              aria-label="Return to menu"
+            />
+          ) : (
+            <SearchButton
+              onClick={() => setView("search")}
+              aria-label="Show search panel"
+            />
           )}
           <CloseButton onClick={handleMenuToggle} aria-label="Close overlay" />
         </div>
