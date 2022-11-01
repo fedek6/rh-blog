ff2aa93e8b8cdfdceb100c91d3b2e878613815ae
Konrad Fedorczyk <fedek6@gmail.com>
2022-11-01 20:07:19 +0100

    Update font in menu

--- components/navbar/MenuDesktop/MenuDesktop.tsx
+++ components/navbar/MenuDesktop/MenuDesktop.tsx
@@ -6,7 +6,7 @@ import { MAIN_MENU } from "@/config/menu";
 
 const Link: ComponentType<HybridLink> = (props) => (
   <HybridLink
-    className="text-d-sm text-eerie-black font-display font-bold p-4 hover:text-english-vermillion dark:text-platinum-300 hover:dark:text-max-yellow-red"
+    className="text-d-sm text-eerie-black font-sans font-bold p-4 hover:text-english-vermillion dark:text-platinum-300 hover:dark:text-max-yellow-red"
     {...props}
   />
 );
