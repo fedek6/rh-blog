29e25f319572fbc0fdcfd699b46670830f6a0239
Konrad Fedorczyk <fedek6@gmail.com>
2022-11-02 22:50:05 +0100

    Add button skeleton component

---
+++ components/common/Button/Button.tsx
@@ -0,0 +1,16 @@
+import tw from "tailwind-styled-components";
+
+export const Button = tw.button`
+  focus:outline-double
+  outline-dark-blue-gray-500
+  dark:outline-max-yellow-red-500
+  cursor-pointer
+  border
+  border-platinum-700
+  dark:border-platinum-600
+  rounded-lg
+  h-10
+  inline-flex
+  justify-center
+  items-center
+`;
