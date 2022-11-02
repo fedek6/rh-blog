d9601a7fc11c795c593782b213c1cf184a5b86e1
Konrad Fedorczyk <fedek6@gmail.com>
2022-11-02 22:50:19 +0100

    Refactor icon button

--- components/common/IconButton/IconButton.tsx
+++ components/common/IconButton/IconButton.tsx
@@ -1,27 +1,6 @@
-import React from "react";
 import tw from "tailwind-styled-components";
+import { Button } from "@/components/common/Button";
 
-const Button = tw.button`
-  focus:outline-double
-  outline-dark-blue-gray-500
-  dark:outline-max-yellow-red-500
-  cursor-pointer
-  border
-  border-platinum-700
-  dark:border-platinum-600
-  rounded-lg
-  h-10
+export const IconButton = tw(Button)`
   w-10
-  inline-flex
-  justify-center
-  items-center
 `;
-
-type Props = {
-  children: React.ReactNode;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
-
-export const IconButton: React.FC<Props> = ({ children, ...props }) => {
-  return <Button {...props}>{children}</Button>;
-};
